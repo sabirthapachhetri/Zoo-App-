b48c480b863493ebcb6b678e3a6e4c0333783f65
Sabir <sabirthapa19@gmail.com>
2022-09-14 20:16:11 +0545

    decoded json file

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Sabir's MacBook on 9/14/22.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
