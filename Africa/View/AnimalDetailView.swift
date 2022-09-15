8c975a334ec9e8650632aeea883cde87c27d9391
Sabir <sabirthapa19@gmail.com>
2022-09-15 16:54:14 +0545

    Added detailed view

---
+++ Africa/View/AnimalDetailView.swift
@@ -0,0 +1,66 @@
+//
+//  AnimalDetailView.swift
+//  Africa
+//
+//  Created by Sabir's MacBook on 9/15/22.
+//
+
+import SwiftUI
+
+struct AnimalDetailView: View {
+    
+    let animal: Animal
+    
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 20) {
+                
+                // HERO IMAGE
+                Image(animal.image)
+                    .resizable()
+                    .scaledToFit()
+                
+                // TITLE
+                Text(animal.name.uppercased())
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .multilineTextAlignment(.center)
+                    .padding(.vertical, 8)
+                    .foregroundColor(.primary)
+                    .background(Color.accentColor
+                        .frame(height: 6)
+                        .offset(y: 24)
+                    )
+                // HEADLINE
+                Text(animal.headline)
+                    .font(.headline)
+                    .multilineTextAlignment(.leading)
+                    .foregroundColor(.accentColor)
+                    .padding(.horizontal)
+                
+                // GALLERY
+                Group {
+                    HeadingView(headingImage: "photo.on.rectangle.angled", headingText: "Wilderness in Pictures")
+                }
+                .padding(.horizontal)
+                
+                // FACTS
+                
+                // DESCRIPTION
+                
+                // MAP
+                
+                // LINK
+            
+            }
+            .navigationBarTitle("Learn about \(animal.name)", displayMode: .inline)
+        }
+    }
+}
+
+struct AnimalDetailView_Previews: PreviewProvider {
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    static var previews: some View {
+        AnimalDetailView(animal: animals[1])
+    }
+}
